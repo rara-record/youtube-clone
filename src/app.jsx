b5d5239cf54163888628d1c4eb0c648a0c55d738
bora <a52447879@gamil.com>
2022-02-18 19:50:55 +0900

    검색기능 콘솔 오류 사항 수정

--- src/app.jsx
+++ src/app.jsx
@@ -13,11 +13,15 @@ const App = () => {
     };
 
     fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyBrswz7pn3CY657vlip_bO0oc1Z_yvlQRo`,
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBrswz7pn3CY657vlip_bO0oc1Z_yvlQRo`,
       requestOptions
     )
       .then((response) => response.json())
-      .then((result) => setVideos(result.items))
+      .then(
+        (result) =>
+          result.items.map((item) => ({ ...item, id: item.id.videoId })) // item.id 가 object형태로 들어와서, key가 똑같다고 error가 나서 item을 변환
+      )
+      .then((items) => setVideos(items))
       .catch((error) => console.log("error", error));
   };
 
