7727d071c18a82e287c7bfed3cdb9de93709c908
bora <a52447879@gamil.com>
2022-02-18 20:51:43 +0900

    검색기능 코드 리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,21 @@
-import React, { useEffect, useState } from "react";
-import styles from "./app.module.css";
-import SearchHeader from "./components/search_header/search_header";
-import VideoList from "./components/video_list/video_list";
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
+import VideoList from './components/video_list/video_list';
 
-const App = () => {
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
 
-  const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBrswz7pn3CY657vlip_bO0oc1Z_yvlQRo`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then(
-        (result) =>
-          result.items.map((item) => ({ ...item, id: item.id.videoId })) // item.id 가 object형태로 들어와서, key가 똑같다고 error가 나서 item을 변환
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+  const search = query => {
+    youtube //
+      .search(query)
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBrswz7pn3CY657vlip_bO0oc1Z_yvlQRo",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube //
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []);
 
   return (
