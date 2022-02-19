6a618175557f97e78b23e77941cd34a92834ff91
bora <a52447879@gamil.com>
2022-02-19 15:30:43 +0900

    video_detail UI 스타일링

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import styles from './video_detail.module.css';
 
 const VideoDetail = ({ video }) => {
-  return <h1>{video.snippet.title}</h1>;
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.video}
+        title={'Youtube Detail Video'}
+        type="text/html"
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <h2>{video.snippet.title}</h2>
+      <h3>{video.snippet.channelTitle}</h3>
+      <pre className={styles.description}>{video.snippet.description}</pre>
+    </section>
+  );
 };
 
 export default VideoDetail;
