0a48241812baa819899f143a82e585223426a793
bora <a52447879@gmail.com>
2022-02-21 23:30:36 +0900

    [feat] : useCallback으로 최적화 (반복적인 함수가 만들어지는 것을 방지)

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
@@ -9,31 +10,40 @@ const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const selectVideo = selectvideo => {
+  const selectVideo = useCallback(selectvideo => {
     setSelectedVideo(selectvideo);
-  };
+  }, []);
 
-  const search = query => {
-    youtube //
-      .search(query)
-      .then(result => {
-        setVideos(result);
-        setSelectedVideo(null);
-      });
-  };
+  const search = useCallback(
+    query => {
+      youtube //
+        .search(query)
+        .then(result => {
+          setVideos(result);
+          setSelectedVideo(null);
+        });
+    },
+    [youtube]
+  );
 
-  const popular = youtube => {
-    youtube //
-      .mostPopular()
-      .then(result => {
-        setVideos(result);
-        setSelectedVideo(null);
-      });
-  };
+  const popular = useCallback(
+    youtube => {
+      youtube //
+        .mostPopular()
+        .then(result => {
+          setVideos(result);
+          setSelectedVideo(null);
+        });
+    },
+    [youtube]
+  );
 
   useEffect(() => {
+    document.cookie = 'safeCookie1=foo; SameSite=Lax';
+    document.cookie = 'safeCookie2=foo';
+    document.cookie = 'crossCookie=bar; SameSite=None; Secure';
     popular(youtube);
-  }, []);
+  }, [youtube]);
 
   return (
     <div className={styles.app}>
